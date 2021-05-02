97ffbc614461848c22117b6f084dec2ff70c3fa4
tamiabosch <tamiabosch@yahoo.de>
2021-05-02 21:59:22 +0200

    chat works

--- app/simplechat.js
+++ app/simplechat.js
@@ -3,6 +3,7 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
+  console.log("User connected")
   ws.on('message', function incoming(data) {
     console.log(data)
     wss.clients.forEach(function each(client) {
--- app/src/main/java/com/example/simplechat/MainActivity.kt
+++ app/src/main/java/com/example/simplechat/MainActivity.kt
@@ -14,38 +14,43 @@ import org.java_websocket.handshake.ServerHandshake
 import java.net.URI
 
 class MainActivity : AppCompatActivity() {
-//    private val chatmessage: EditText? = findViewById(R.id.chatmessage)
-//    private val username: EditText? = findViewById(R.id.username)
-//    private val send: Button? = findViewById(R.id.send)
-//    private val chattext: TextView? = findViewById(R.id.chattext)
-
+    private lateinit var chatmessage: EditText
+    private lateinit var username: EditText
+    private lateinit var send: Button
+    private lateinit var chattext: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Websocket öffnen
         val myWebSocketClient = MyWebSocketClient(URI("ws://10.0.2.2:8080"), Draft_17())
         myWebSocketClient.connect()
-        //Elemente holen
-        val send: Button = findViewById(R.id.send)
-        val username: EditText = findViewById(R.id.username)
-        val chatmessage: EditText = findViewById(R.id.chatmessage)
 
-        send.setOnClickListener {
-            val myUsername = username.text
-            val myMessage = chatmessage.text
+        //Elemente holen
+        chatmessage = findViewById(R.id.chatmessage)
+        send = findViewById(R.id.send)
+        username = findViewById(R.id.username)
+        chattext = findViewById(R.id.chattext)
+
+        this.send.setOnClickListener {
+            val myUsername = this@MainActivity.username.text
+            val myMessage = this@MainActivity.chatmessage.text
+            Log.d("send", "clicked")
             myWebSocketClient.send("$myUsername: $myMessage")
         }
 
     }
+
     inner class MyWebSocketClient(serverUri: URI?, draft: Draft?) : WebSocketClient(serverUri, draft) {
         override fun onError(ex: Exception?) {
             Toast.makeText(this@MainActivity, "Error", Toast.LENGTH_LONG).show()
         }
         override fun onOpen(handshakedata: ServerHandshake?) {
             this@MainActivity.runOnUiThread {
-                 Toast.makeText(this@MainActivity, "Opened", Toast.LENGTH_LONG).show()
+                Toast.makeText(this@MainActivity, "Opened connection", Toast.LENGTH_LONG).show()
+                Log.d("MainActivity", "Opened")
             }
         }
         override fun onClose(code: Int, reason: String?, remote: Boolean) {
@@ -54,7 +59,8 @@ class MainActivity : AppCompatActivity() {
         override fun onMessage(message: String?) {
             val chattext: TextView = findViewById(R.id.chattext)
             val oldText = chattext.text
-            chattext.text = "$message\n$oldText"
+            this@MainActivity.chattext.text = "$message\n$oldText"
+
         }
     }
 
